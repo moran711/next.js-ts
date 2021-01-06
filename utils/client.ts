6b86b03aa071b0fc1d2f273fcc4b60734ba1ac3b
Roman <romaniv788@gmail.com>
2021-01-06 09:10:36 +0200

    add process env variable

--- utils/client.ts
+++ utils/client.ts
@@ -2,7 +2,7 @@ import ApolloClient from "apollo-boost";
 import { InMemoryCache } from "apollo-cache-inmemory";
 
 export const client = new ApolloClient({
-  uri: 'http://localhost:3000/api/graphql',
+  uri: process.env.SERVER_URI,
   cache: new InMemoryCache({ addTypename: false }),
 });
 ``;
